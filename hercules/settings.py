65b53eac9d212a9221c42e0d4ef71f2e7bb44341
Vitor Wdson <vitor.wdson2@gmail.com>
2023-09-21 22:27:19 -0400

    Created the user app and model

--- hercules/settings.py
+++ hercules/settings.py
@@ -81,6 +81,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -152,6 +153,7 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+AUTH_USER_MODEL = "users.User"
 
 
 # Logging
