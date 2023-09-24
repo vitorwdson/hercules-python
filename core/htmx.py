de9bbe817a20759e33d68588faf6513cfa1bb9ba
Vitor Wdson <vitor.wdson2@gmail.com>
2023-09-24 13:35:18 -0400

    Formatting

--- core/htmx.py
+++ core/htmx.py
@@ -1,18 +1,17 @@
 import json
+from core.typing import HttpRequest, HttpResponse
 from django.shortcuts import render
 from typing import Any
-
-from core.typing import HttpRequest, HttpResponse
 
 
 def render_htmx(
     request: HttpRequest,
     template_name: str,
-    context: dict[str, Any]|None = None,
-    custom_full_template_name: str|None = None,
-    content_type: str|None = None,
-    status: int|None = None,
-    using: str|None = None,
+    context: dict[str, Any] | None = None,
+    custom_full_template_name: str | None = None,
+    content_type: str | None = None,
+    status: int | None = None,
+    using: str | None = None,
 ):
     if context is None:
         context = {}
@@ -28,7 +27,7 @@ def render_htmx(
     if context is None:
         context = {}
 
-    context['partial_content_template'] = template_name
+    context["partial_content_template"] = template_name
 
     return render(
         request,
@@ -39,18 +38,20 @@ def render_htmx(
         using,
     )
 
-def show_message(response: HttpResponse|None = None, icon: str|None = None, message: str|None = None) -> HttpResponse:
+
+def show_message(
+    response: HttpResponse | None = None,
+    icon: str | None = None,
+    message: str | None = None,
+) -> HttpResponse:
     if response is None:
-        response = HttpResponse('')
+        response = HttpResponse(b"")
 
     if icon is None and message is None:
         response.headers["HX-Trigger"] = "form:showMessage"
     else:
-        response.headers["HX-Trigger"] = json.dumps({
-            "form:showMessage": {
-                'icon': icon,
-                'message': message
-            }
-        })
+        response.headers["HX-Trigger"] = json.dumps(
+            {"form:showMessage": {"icon": icon, "message": message}}
+        )
 
     return response
