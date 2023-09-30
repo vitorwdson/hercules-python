53858504e7934796c3aaa226f13cb0ef5abf54fa
Vitor Wdson <vitor.wdson2@gmail.com>
2023-09-30 13:48:54 -0400

    Fontawesome, placeholders and base layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './{core}/templates/**/*.html',
+    './**/templates/**/*.html',
   ],
   theme: {
     extend: {},
