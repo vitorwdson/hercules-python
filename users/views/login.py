74c914f1b54eac884e90bf5b709fd774e4dc337b
Vitor Wdson <vitor.wdson2@gmail.com>
2023-10-01 03:25:28 -0400

    Hiding sidebar on login page

--- users/views/login.py
+++ users/views/login.py
@@ -3,12 +3,19 @@ from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.views import LoginView
 from django.shortcuts import redirect
 from django_htmx.http import HttpResponseClientRedirect
+from typing import Any
 
 from core.typing import HttpRequest
 
 
 class Login(LoginView):
-    template_name = 'users/login.html'
+    template_name = "users/login.html"
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context.update({'hide_sidebar': True})
+
+        return context
 
 
 def logout(request: HttpRequest):
