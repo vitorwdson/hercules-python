028489331b8b6711c115d7570eefc8de2347f922
Vitor Wdson <vitor.wdson2@gmail.com>
2023-10-08 15:58:57 -0400

    Separate index template

--- projects/views/index.py
+++ projects/views/index.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
 from core.typing import HttpRequest
+from core.htmx import render_htmx
 from users.decorators import login_required, project_required
 
 @login_required
 @project_required
 def index(request: HttpRequest):
-    return render(request, 'base.html')
+    return render_htmx(request, 'projects/index.html')
