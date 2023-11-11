7f6929b6cad191983aaaa74f6b9a784a768ddb7d
Vitor Wdson <vitor.wdson2@gmail.com>
2023-11-11 16:10:37 -0400

    Issues list

--- issues/urls.py
+++ issues/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name = 'issues'
 urlpatterns = [
-    path('issues', views.issue_list.list_view, name='list'),
+    path('issues', views.issue_list.IssueList.as_view(), name='list'),
     path('new', views.issue.new, name='new'),
 ]
--- issues/views/issue_list.py
+++ issues/views/issue_list.py
@@ -1,9 +1,37 @@
+from typing import Any
+from django.db.models import Model
+from django.utils.decorators import method_decorator
+from django.views.generic.list import ListView
 from core.htmx import render_htmx
 from core.typing import HttpRequest
+from issues.models import Issue
 from users.decorators import login_required, project_required
 
 
-@login_required
-@project_required
-def list_view(request: HttpRequest):
-    return render_htmx(request, "issues/list.html")
+class IssueList(ListView):
+    request: HttpRequest
+    model: type[Model] = Issue
+    paginate_by = 15
+    allow_empty = True
+    template_name: str = "issues/list.html"
+    ordering = ["-created_at"]
+
+    def render_to_response(self, context: dict[str, Any], **_: Any):
+        return render_htmx(self.request, self.template_name, context)
+
+    @method_decorator(login_required)
+    @method_decorator(project_required)
+    def get(self, request: HttpRequest, *args: Any, **kwargs: Any):
+        return super().get(request, *args, **kwargs)
+
+    def get_queryset(self):
+        qs = self.model.objects
+        qs = qs.filter(project=self.request.selected_project.project)
+
+        ordering = self.get_ordering()
+        if ordering:
+            if isinstance(ordering, str):
+                ordering = (ordering,)
+            qs = qs.order_by(*ordering)
+
+        return qs.distinct()
